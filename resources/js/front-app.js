b75c00510c7a27f6b0bda2fb99ede33825bfc652
Sonicastra <Sonicastra@telenet.be>
2021-07-14 18:14:48 +0200

    added to this github

---
+++ resources/js/front-app.js
@@ -0,0 +1,7 @@
+require('./bootstrap.js')
+/*require('nouislider');*/
+require('../../resources/assets/front_assets/js/front-script.js');
+require('@fortawesome/fontawesome-free/js/fontawesome.min.js');
+
+
+
